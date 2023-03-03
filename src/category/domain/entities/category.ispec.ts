2d65d8b0b31fa1dd88d7dc8f47808d759ce864fa
WagnerReis <wagnerreis1997@gmail.com>
2023-03-03 07:37:23 -0300

    Teste constructor da entidade categoria

--- src/category/domain/entities/category.ispec.ts
+++ src/category/domain/entities/category.ispec.ts
@@ -2,7 +2,7 @@ import Category from "./category";
 
 describe("Category Unit tests", () => {
   test("contructor of category", () => {
-    const category = new Category("Movie");
+    const category = new Category({name: "Movie"});
 
     expect(category.name).toBe("Movie");
   });
--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,8 +2,19 @@ import Category from "./category";
 
 describe("Category Unit tests", () => {
   test("contructor of category", () => {
-    const category = new Category("Movie");
+    const created_at = new Date();
+    const category = new Category({
+      name: "Movie",
+      description: "description",
+      is_active: true,
+      created_at,
+    });
 
-    expect(category.name).toBe("Movie");
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "description",
+      is_active: true,
+      created_at,
+    });
   });
 });
