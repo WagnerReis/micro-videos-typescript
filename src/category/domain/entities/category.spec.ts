b913e38f43f4c5a51bf145f6bcfbe1c827b8f9ec
WagnerReis <wagnerreis1997@gmail.com>
2023-03-03 08:15:10 -0300

    Teste lógica de atribuição das propriedades facultativas

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,18 +1,68 @@
 import Category from "./category";
+import { omit } from "lodash";
 
 describe("Category Unit tests", () => {
   test("contructor of category", () => {
-    const created_at = new Date();
-    const category = new Category({
+    let category = new Category({
       name: "Movie",
-      description: "description",
+    });
+
+    let props = omit(category.props, "created_at");
+
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: null,
       is_active: true,
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+
+
+    category = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+    });
+    let created_at = new Date();
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
       created_at,
     });
 
-    expect(category.props).toStrictEqual({
+
+
+    category = new Category({
       name: "Movie",
-      description: "description",
+      description: "other description",
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "other description",
+    });
+
+
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
+
+    
+
+    created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+      created_at,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
       is_active: true,
       created_at,
     });
