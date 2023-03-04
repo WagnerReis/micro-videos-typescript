de20404f5ca2dcbc9b2015cfd257a0b1f2f60d55
WagnerReis <wagnerreis1997@gmail.com>
2023-03-04 13:43:16 -0300

    Teste Objeto de valor UniqueEntityId parte 2

--- src/@seedwork/errors/unique-entity-id.vo.spec.ts
+++ src/@seedwork/errors/unique-entity-id.vo.spec.ts
@@ -1,4 +1,5 @@
 import UniqueEntityId from "../domain/unique-entity-id.vo";
+import { validate as uuidValidate  } from "uuid";
 
 describe("UniqueEntityId Unit Test", () => {
   it("should throw error when uuid is invalid", () => {
@@ -6,6 +7,21 @@ describe("UniqueEntityId Unit Test", () => {
     expect(() => {
       new UniqueEntityId("fake id");
     }).toThrow("ID must be a valid UUID");
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const uuid = "7f313b2a-886a-4fc2-9164-e809a1929585";
+    const vo = new UniqueEntityId(uuid);
+    expect(vo.id).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const vo = new UniqueEntityId();
+    expect(uuidValidate(vo.id)).toBeTruthy();
     expect(validateSpy).toHaveBeenCalled();
   });
 });
